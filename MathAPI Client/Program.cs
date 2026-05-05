9e3acfd4d4b23a14271d949452d659d1d49a3479
EMJulianCorrea <ST10438910@myemeris.edu.za>
2026-05-05 14:06:06 +0200

    CI workflow completed

--- MathAPI Client/Program.cs
+++ MathAPI Client/Program.cs
@@ -23,7 +23,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseSession();
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseStaticFiles();
 
 app.UseRouting();
